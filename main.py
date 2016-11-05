0c70cb2047df51de4ace745e0a3f09f59d9f02bd
Ben Campbell <ben.in.campbell@gmail.com>
2016-11-05 17:51:12 -0400

    Adjust cron job
    Previously the cron job sent a reminder email to all users every 24 hours. The cron job now sends a reminder email only to users who have at least one active game.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import logging
 import webapp2
 from google.appengine.api import mail, app_identity
 from pelmanism_api import PelmanismApi
-from models import User
+from models import User, Game
 
 
 # Cron job for sending out a reminder email
@@ -16,15 +16,22 @@ class SendReminderEmail(webapp2.RequestHandler):
 		"""Add docstring"""
 		app_id = app_identity.get_application_id()
 		users = User.query(User.email != None)
+		msg = ('\n\nIt looks like you started a Pelmanism game, '
+			'but haven\'t finished. Come back and find some matches!')
 		for user in users:
-			subject = 'This is a reminder!'
-			body = 'Hello {}, try out Pelmanism. It\'s fun!'.format(
-				user.name)
-			# This will send test emails to all users
-			mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
-				user.email,
-				subject,
-				body)
+			games = Game.query(Game.user == user.key).fetch()
+			for g in games:
+				if g.game_over == False:
+					if g.cancelled == False:
+						subject = 'Where did you go?'
+						body = 'Hi, {}!'.format(user.name) + msg
+						# This will send test emails to all users
+						mail.send_mail(
+							'noreply@{}.appspotmail.com'.format(app_id),
+							user.email,
+							subject,
+							body)
+						break
 
 
 # Task queue for updating average moves remaining
