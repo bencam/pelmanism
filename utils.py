e0444ae308910ced5be41485e6ba05c9214fecbd
Ben Campbell <ben.in.campbell@gmail.com>
2016-11-08 20:53:34 -0500

    Polish up main.py and utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,31 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+"""
+The utils.py file provides a function which is used in pelmanism_api.py
+to retrieve game information.
+
+"""
+
 
 import logging
 from google.appengine.ext import ndb
 import endpoints
 
+
 def get_by_urlsafe(urlsafe, model):
-	"""Add docstring"""
+	"""Returns the ndb.model entity that is connected to the urlsafe key;
+	checks to ensure that the type of entity returned is of the correct
+	kind; raises an error if the key string is (a) incorrect or (b)
+	of the wrong kind
+	
+	Args:
+		urlsafe: a urlsafe key string
+		model: the expected entity kind
+	Returns:
+		The entity that is connected to the urlsafe key OR None if
+		an entity does not exist
+	Raises:
+		ValueError"""
 	try:
 		key = ndb.Key(urlsafe=urlsafe)
 	except TypeError:
