0b05555a5bfaa7ab7a1e85347f93f68ff6f6a513
Ben Campbell <ben.in.campbell@gmail.com>
2016-12-30 19:33:52 -0500

    Remove User class in game module

--- models/game.py
+++ models/game.py
@@ -7,29 +7,12 @@ Add docstring
 """
 
 
-# Add imports
+from datetime import datetime
+from protorpc import messages
+from google.appengine.ext import ndb
 
 
 # Define game objects
-class User(ndb.Model):
-    """User profile"""
-    name = ndb.StringProperty(required=True)
-    email = ndb.StringProperty()
-    games_played = ndb.IntegerProperty(required=True)
-    total_attempts = ndb.IntegerProperty(required=True)
-    total_points = ndb.IntegerProperty(required=True)
-    points_per_attempt = ndb.IntegerProperty(required=True)
-
-    def to_rankings_form(self):
-        """Return a UserRanking representation of the User"""
-        return UserRanking(
-            user_name=self.name,
-            games_played=self.games_played,
-            total_attempts=self.total_attempts,
-            total_points=self.total_points,
-            points_per_attempt=self.points_per_attempt)
-
-
 class Game(ndb.Model):
     """Game object"""
     deck = ndb.StringProperty(repeated=True)
